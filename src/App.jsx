f13ef3ac35320686c8dee0e9de90a7a550c61821
visinodeveloper <visinodeveloper@gmail.com>
2024-06-24 01:31:28 -0300

    refactoring?

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 
-import { Routes ,Route} from 'react-router-dom';
 import "./app.css"
 import Game from "./components/Game/Game"
 import Header from "./components/Header/Header"
@@ -7,21 +6,23 @@ import Listado from "./components/Listado/Listado"
 import QuestionForm from './components/Forms/QuestionForm';
 import ThemeForm from './components/Forms/ThemeForm'
 import Footer from "./components/Footer/Footer"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
-  
   return (
-    <div className="container app">
-      <Header />
-      <Routes>
-        <Route path="/Quizz-client-production/" element={<Listado />} />
-        <Route path="/Quizz-client-production/game/:gameId" element={<Game />} />
-        <Route path="/Quizz-client-production/game/:gameId/add-question" element={<QuestionForm />} />
-        <Route path="/Quizz-client-production/add-theme" element={<ThemeForm />} />
-      </Routes>
-      <Footer />
-    </div>
-  );  
+    <Router basename="/Quizz-client-production">
+      <div className="container app">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Listado />} />
+          <Route path="/game/:gameId" element={<Game />} />
+          <Route path="/game/:gameId/add-question" element={<QuestionForm />} />
+          <Route path="/add-theme" element={<ThemeForm />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
